76c00e905a1f90760df832f1a613a21560835f36
Simone <simonedambrgio@live.it>
2020-02-21 16:10:45 -0500

    up

--- R/GSA_aDDM.R
+++ R/GSA_aDDM.R
@@ -1,17 +1,14 @@
 ####################################### GSA_mle #######################################
 #Gried-Search Algorithm Function
-GSA_aDDM <- function(data, d_set, sigma_set, theta_set, timeStep = 10, barrier = 1, numCores){
+GSA_aDDM <- function (data, d_set, sigma_set, theta_set, timeStep = 10, barrier = 1, numCores) {
   
-#mle function
-mle <- function (data, d, sigma, theta, timeStep = 10, barrier = 1) {
-    
+  mle <- function(data, d, sigma, theta, timeStep = 10, barrier = 1) {
     library(foreach)
     library(doParallel)
     library(Rcpp)
-    
-    get_trial_likelihood_C <- function(value_up_boundary, value_down_boundary, 
-                                       d, theta, sigma, timeStep, approxStateStep = 0.1, barrier, 
-                                       choice, FixItem, FixTime) {
+    get_trial_likelihood_C <- function(value_up_boundary, 
+                                       value_down_boundary, d, theta, sigma, timeStep, approxStateStep = 0.1, 
+                                       barrier, choice, FixItem, FixTime) {
       correctedFixTime <- FixTime%/%timeStep
       if (sum(correctedFixTime) < 1) 
         stop("fix_time più piccolo del timeStep")
@@ -79,9 +76,11 @@ mle <- function (data, d, sigma, theta, timeStep = 10, barrier = 1) {
                                                   sigma = sigma, choice = unique(data[data$trial == 
                                                                                         trial_i, "choice"]), FixItem = data[data$trial == 
                                                                                                                               trial_i, "fix_item"], FixTime = data[data$trial == 
-                                                                                                                                                                     trial_i, "fix_time"], timeStep = timeStep, barrier = barrier))
+                                                                                                                                                                     trial_i, "fix_time"], timeStep = timeStep, 
+                                                  barrier = barrier))
     nll <- -sum(log(likelihood[likelihood != 0]))
-    print(paste0("Calcolo NLL modello: d = ", d, " sigma = ", sigma, " theta = ", theta, " --- NLL = ", nll))
+    print(paste0("Calcolo NLL modello: d = ", d, " sigma = ", 
+                 sigma, " theta = ", theta, " --- NLL = ", nll))
     return(nll)
   }
   
@@ -89,58 +88,36 @@ mle <- function (data, d, sigma, theta, timeStep = 10, barrier = 1) {
   improvement <- 1
   iteration <- 1
   
-while (improvement >= 0.01){
-    
+  while (improvement >= 0.01) {
     print(paste("Performing step", iteration))
-    
-    #Set o parameters to test
-    par_set <- expand.grid(d=d_set, sigma=sigma_set, theta=theta_set)
-    #Compute the NLL for each set of parameters
-    grid_results <- sapply( 1:nrow(par_set), function(row_i){
-      mle(data=data, d=par_set[row_i, 'd'], sigma=par_set[row_i, 'sigma'], theta=par_set[row_i, 'theta'],
-          timeStep=timeStep, barrier = barrier)
+    par_set <- expand.grid(d = d_set, sigma = sigma_set, 
+                           theta = theta_set)
+    grid_results <- sapply(1:nrow(par_set), function(row_i) {
+      mle(data = data, d = par_set[row_i, "d"], sigma = par_set[row_i, 
+                                                                "sigma"], theta = par_set[row_i, "theta"], timeStep = timeStep, 
+          barrier = barrier)
     })
-    
-    #Best set of parameters
-    Best_par_set <- par_set[ which(grid_results==min(grid_results)), ]
-    
+    Best_par_set <- par_set[which(grid_results == min(grid_results)), 
+                            ]
     Best_NLL_t1 <- min(grid_results)
-    
-    if(iteration==1) improvement <- 1 else  improvement <- Best_NLL_t0/Best_NLL_t1-1 
-    
-    iteration <- iteration+1
+    if (iteration == 1) 
+      improvement <- 1
+    else improvement <- Best_NLL_t0/Best_NLL_t1 - 1
+    iteration <- iteration + 1
     Best_NLL_t0 <- Best_NLL_t1
-    
-    #Set delta
     delta_d <- diff(d_set) %>% min()
     delta_sigma <- diff(sigma_set) %>% min()
     delta_theta <- diff(theta_set) %>% min()
     
-    #Set new set of parameters
-    d_set <- c(Best_par_set$d - (delta_d/2), Best_par_set$d, Best_par_set$d + (delta_d/2))
-    sigma_set <- c(Best_par_set$sigma - (delta_sigma/2), Best_par_set$sigma, Best_par_set$sigma + (delta_sigma/2))
-    theta_set <- c(Best_par_set$theta - (delta_theta/2), Best_par_set$theta, Best_par_set$theta + (delta_theta/2))
+    #new d_set
+      d_set <- c(Best_par_set$d - (delta_d/2), Best_par_set$d, Best_par_set$d + (delta_d/2))
+    
+    #new sigma_set
+      sigma_set <- c(Best_par_set$sigma - (delta_sigma/2), Best_par_set$sigma, Best_par_set$sigma + (delta_sigma/2))
+    
+    #new theta_set
+      theta_set <- c(Best_par_set$theta - (delta_theta/2), Best_par_set$theta, Best_par_set$theta + (delta_theta/2))
   }
   
   return(Best_par_set)
-  
 }
-
-#Load data
-#data <- read.csv('Data/aDDM_data.csv')
-#fix_type <- fixations_type( fixations = data )
-
-#Vengono Prodotti 400 trial
-#fix_sim <- raDDM(n = 5, sigma = 0.08, theta = 0.7, V = 0, d = 0.00065, 
-#                 value_up_boundary = seq(1,20,2), value_down_boundary = seq(1,20,2), 
-#                 first_fixation_time_up = fix_type$first_fixation_time_up%/%10, 
-#                 first_fixation_time_down = fix_type$first_fixation_time_down%/%10, 
-#                 fixation_time_up = fix_type$fixation_time_up%/%10, 
-#                 fixation_time_down = fix_type$fixation_time_down%/%10, 
-#                 first_fix_type = fix_type$first_fix_type, 
-#                 trans_time = fix_type$trans_time[fix_type$trans_time<100]%/%10,
-#                 non_dec_time = fix_type$non_dec_time%/%10,
-#                 num_up_boundary = 1) %>%
-#  mutate(subject = 0, rt = rt*10, fix_time = fix_time*10)
-
-#GSA_mle(data = fix_sim, d_set = c(0.001, 0.005, 0.01), sigma_set = c(0.01, 0.05, 0.1), theta_set = c(0.1, 0.5, 0.9))
